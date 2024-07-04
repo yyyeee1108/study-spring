7ea294f73f8a4914e068718622da8f3684c3a954
Yelim Shin <yyyeee1108@gmail.com>
2024-07-05 02:25:51 +0900

    스프링 시큐리티 기초 - 프로젝트 생성
    View : mustache
    Spring Security 의존성 추가
    Lombok : 애너테이션 기반 코드 자동완성 라이브러리
    spring-boot-starter-web : Spring 웹 어플리케이션 개발을 위한 의존성 추가
    DB : MySQL
    spring-boot-starter-data-jpa : Spring Data JPA 사용을 위한 의존성 추가

---
+++ SecuritySession/src/main/java/com/example/testsecurity/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.example.testsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String mainP() {
+        return "main";
+    }
+}
