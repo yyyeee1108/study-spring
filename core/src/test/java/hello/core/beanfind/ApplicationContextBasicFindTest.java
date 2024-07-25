ada9282011d4c2add8ec801886c77f0793df8218
Yelim Shin <yyyeee1108@gmail.com>
2024-07-26 01:42:15 +0900

    [스프링 핵심 원리 기본] - 스프링 빈 조회 기본

---
+++ core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,52 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+
+    /*
+    아래 코드와 같이 역할이 아닌 구현에 초점을 맞추는 코드는 일반적인 상황에서 좋지 못한 코드이다
+    역할에 초점을 맞추는 것이 좋다
+    구체 타입으로 조회하면 유연성이 떨어진다
+     */
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanName2() {
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX() {
+        assertThrows(NoSuchBeanDefinitionException.class,
+                ()-> ac.getBean("xxxxxxx", MemberService.class));
+    }
+}
