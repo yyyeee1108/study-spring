0e7c68626f2e630bc792840b43194b6e95fb91ee
Yelim Shin <yyyeee1108@gmail.com>
2024-08-29 21:21:13 +0900

    [스프링 핵심 원리 기본] - 빈 생명주기 콜백: @Bean에서 초기화, 소멸 메서드 지정
    - @Bean(initMethod = init, destroyMethod = close) 처럼 빈 등록하며 직접 초기화, 소멸 메서드 지정 가능
    - 스프링 빈 클래스가 스프링 코드에 의존하지 않음. 설정 정보 사용하는 것임 -> 외부 라이브러리에도 적용 가능

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,7 +3,7 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -11,7 +11,7 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("생성자 호출, url = " + url);
     }
 
-    public void setUrl(String url){
+    public void setUrl(String url) {
         this.url = url;
     }
 
@@ -25,22 +25,20 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     }
 
     // 서비스 종료 시 호출
-    public void disconnect(){
+    public void disconnect() {
         System.out.println("close: " + url);
     }
 
     // 의존 관계 주입이 끝나면 호출하는 메서드
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+    public void init() throws Exception {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
     // 빈 종료 전 호출되는 메서드
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
